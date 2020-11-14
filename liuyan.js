2fefb99ed2ce1b4786a61f5f51ca905311dbde19
liu1314521 <40618013+liu1314521@users.noreply.github.com>
2020-11-14 15:09:06 +0800

    Create liuyan.js

---
+++ liuyan.js
@@ -0,0 +1,150 @@
+document.write(" <ul class=\"Con197_HomePro\">");
+document.write("            <li><a rel=\"nofollow\" href=\"\/products\/jaw-crusher.html\" class=\"Con197_HomeProPic\">");
+document.write("                <img src=\"\/proimg\/1_20170317044932420b.jpg\" alt=\"Jaw Crusher\" \/><\/a>");
+document.write("                <h3><a rel=\"nofollow\" href=\"\/products\/jaw-crusher.html\" class=\"moblongTailultit\">Jaw Crusher<\/a> <\/h3>");
+document.write("            <\/li>");
+document.write("            <li><a rel=\"nofollow\" href=\"\/products\/impact-crusher.html\" class=\"Con197_HomeProPic\">");
+document.write("                <img src=\"\/proimg\/1_20170317044651585b.jpg\" alt=\"Impact Crusher\" \/><\/a>");
+document.write("                <h3><a rel=\"nofollow\" href=\"\/products\/impact-crusher.html\" class=\"moblongTailultit\">Impact Crusher<\/a> <\/h3>");
+document.write("            <\/li>");
+document.write("            <li><a rel=\"nofollow\" href=\"\/products\/mobile-crushers.html\" class=\"Con197_HomeProPic\">");
+document.write("                <img src=\"\/proimg\/1_20170317044326496b.jpg\" alt=\"Mobile Crushers\" \/><\/a>");
+document.write("                <h3><a rel=\"nofollow\" href=\"\/products\/mobile-crushers.html\" class=\"moblongTailultit\">Mobile Crushers<\/a><\/h3>");
+document.write("            <\/li>");
+document.write("            <li><a rel=\"nofollow\" href=\"\/products\/vsi-crusher.html\" class=\"Con197_HomeProPic\">");
+document.write("                <img src=\"\/proimg\/1_20170317044015297b.jpg\" alt=\"Vsi Crusher\" \/><\/a>");
+document.write("                <h3><a rel=\"nofollow\" href=\"\/products\/vsi-crusher.html\" class=\"moblongTailultit\">Vsi Crusher<\/a> <\/h3>");
+document.write("            <\/li>");
+document.write("            <li><a rel=\"nofollow\" href=\"\/products\/sand-washing-machine.html\" class=\"Con197_HomeProPic\">");
+document.write("                <img src=\"\/proimg\/1_20170317043919320b.jpg\" alt=\"Sand Washing Machine\" \/><\/a>");
+document.write("                <h3><a rel=\"nofollow\" href=\"\/products\/sand-washing-machine.html\" class=\"moblongTailultit\">Sand Washing Machine<\/a> <\/h3>");
+document.write("            <\/li>");
+document.write("            <li><a rel=\"nofollow\" href=\"\/products\/ball-mill.html\" class=\"Con197_HomeProPic\">");
+document.write("                <img src=\"\/proimg\/2_20170317043818414b.jpg\" alt=\"Ball Mill\" \/><\/a>");
+document.write("                <h3><a rel=\"nofollow\" href=\"\/products\/ball-mill.html\" class=\"moblongTailultit\">Ball Mill<\/a> <\/h3>");
+document.write("            <\/li>");
+document.write("            <li><a rel=\"nofollow\" href=\"\/products\/single-cylinder-cone-crusher.html\" class=\"Con197_HomeProPic\">");
+document.write("                <img src=\"\/proimg\/1_20170815035227648b.jpg\" alt=\"Single Cylinder Cone Crusher\"><\/a>");
+document.write("                <h3><a rel=\"nofollow\" href=\"\/products\/single-cylinder-cone-crusher.html\" class=\"moblongTailultit\">Single Cylinder Cone Crusher<\/a><\/h3>");
+document.write("            <\/li>");
+document.write("            <li><a rel=\"nofollow\" href=\"\/products\/dry-powder-briquetting-machine.html\" class=\"Con197_HomeProPic\">");
+document.write("                <img src=\"\/proimg\/7_20161106075554908s.jpg\" alt=\"Dry Powder Briquetting Machine\"\/><\/a>");
+document.write("                <h3><a rel=\"nofollow\" href=\"\/products\/dry-powder-briquetting-machine.html\" class=\"moblongTailultit\">Dry Powder Briquetting Machine<\/a><\/h3>");
+document.write("            <\/li>");
+document.write("        <\/ul>");
+document.write(" <div class=\"lyb-msg m-top p-padding clearfix\" id=\"lyy\">");
+document.write("            <div class=\"msgfl\">");
+document.write("                <div class=\"mag-top\">");
+document.write("                    <p class=\"p1\">");
+document.write("                        Leave Us Message");
+document.write("                    <\/p>");
+document.write("                    <p class=\"p2\">30s quick response service <span><i><\/i><\/span><\/p>");
+document.write("                <\/div>");
+document.write("                <p class=\"p3\">Undecided? Fill in your needs immediately and our engineer will send you professional solution.<\/p>");
+document.write("<form id=\"Inquiry\" name=\"Inquiry\" class=\"send\" method=\"post\" action=\"https:\/\/q.kssbchina.com\/information\/\" onsubmit=\"return doSubmits()\">");
+document.write("    <div class=\"input-panel\">");
+document.write("        <div class=\"me one\">");
+document.write("            <strong>Product Name: <\/strong>");
+document.write("            <input id=\"productname\" name=\"productname\" class=\"txt icon-text-pro\" placeholder=\"Product Name\" type=\"text\">");
+document.write("        <\/div>");
+document.write("        <div class=\"me two\">");
+document.write("            <strong>Your Name:<\/strong>");
+document.write("            <input id=\"name\" name=\"name\" class=\"txt icon-text-user\" placeholder=\"Your Name\" type=\"text\">");
+document.write("        <\/div>");
+document.write("        <div class=\"me three\">");
+document.write("            <strong>Your Email(<span style=\"margin: 0px; color: red\">*<\/span>)<\/strong>");
+document.write("            <input id=\"email\" name=\"email\" class=\"txt icon-text-email\" placeholder=\"Your Email(*)\" type=\"text\">");
+document.write("        <\/div>");
+document.write("        <div class=\"me four\">");
+document.write("            <strong>Your Phone or Whatsapp:<\/strong>");
+document.write("            <input id=\"telephone\" name=\"telephone\" class=\"txt icon-text-tel\" placeholder=\"Your Phone or Whatsapp\" type=\"text\">");
+document.write("        <\/div>");
+document.write("    <\/div>");
+document.write("    <div class=\"me five\">");
+document.write("        <strong>Message(<span style=\"margin: 0px; color: red\"> as specific as possible*<\/span>)<\/strong>");
+document.write("        <textarea id=\"message\" class=\"textarea icon-text-message\" name=\"message\" placeholder=\"Equipment name, model, related issues(*)\"><\/textarea>");
+document.write("    <\/div>");
+document.write("    <input id=\"site_feedback\" type=\"hidden\" name=\"site_feedback\">");
+document.write("    <input id=\"from_way\" type=\"hidden\" name=\"from_way\">");
+document.write("    <input id=\"refer\" type=\"hidden\" name=\"refer\">");
+document.write("    <div class=\"PorConSixXX\">");
+document.write("        <p>");
+document.write("            <input type=\"checkbox\" checked=\"checked\" class=\"choose_input\"><\/p>");
+document.write("        I accept the Data Protection Declaration");
+document.write("    <\/div>");
+document.write("    <input id=\"button_sub\" name=\"button_sub\" class=\"btn-primary\" type=\"submit\" value=\"Send Now!\">");
+document.write("<\/form>");
+document.write("            <\/div>");
+document.write("            <div class=\"msgfr\">");
+document.write("                <p class=\"lyb-fr-img\">");
+document.write("                    <img src=\"\/fourteen\/images\/1-lybmsg-fr.png\" alt=\"1-lybmsg-fr\" \/>");
+document.write("                <\/p>");
+document.write("                <p class=\"lyb-fr-btn\">Service hotline: <span class=\"swt more\">Consult immediately<\/span><\/p>");
+document.write("                <p class=\"lyb-fr-btn\">Headquarters address: Zhengzhou, China<\/p>");
+document.write("            <\/div>");
+document.write("        <\/div>");
+
+function doSubmits() {
+    var stremail = document.getElementById("email");
+    if (stremail.value == "") {
+        alert('Please write down your correct Email');
+        stremail.focus();
+        return false;
+    } else {
+        var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+        isok = reg.test(stremail.value);
+        if (!isok) {
+            alert("The format of the e-mail is incorrect, please re-enter the e-mail！");
+            stremail.focus();
+            return false;
+        }
+    }
+
+    var smessage = document.getElementById("message");
+    if (smessage.value == "") {
+        smessage.style.backgroundColor = "#bbc0c6";
+        alert('Please fill in the message');
+        smessage.focus();
+        return false;
+    }
+    var phone = document.getElementById("telephone");
+    if (phone.value !== "") {
+        var regs = /^[0-9]*$/;
+        isok = regs.test(phone.value);
+        leng = phone.value.length;
+        if (!isok || leng>20) {
+            alert("Please fill in the correct mobile number or Whatsapp;");
+            phone.focus();
+            return false;
+        }
+    }
+    // 1.客户端浏览器时间
+    var myDate = new Date();
+    var datenow = myDate.toLocaleDateString() + "-" + myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+    //2. 随即字符串
+    var str = '';
+    var length = 10;
+    for (; str.length < length; str += Math.random().toString(36).substr(2));
+    str = str.substr(0, length);
+
+    //3.移动版
+    var mobileAgent = new Array("iphone", "ipod", "ipad", "android", "mobile", "blackberry", "webos", "incognito",
+        "webmate", "bada", "nokia", "lg", "ucweb", "skyfire");
+    var browser = navigator.userAgent.toLowerCase();
+    var isMobile = "pc";
+    for (var i = 0; i < mobileAgent.length; i++) {
+        if (browser.indexOf(mobileAgent[i]) != -1) {
+            isMobile = "m";
+            break;
+        }
+    }
+    window.document.Inquiry.site_feedback.value = str;
+    window.document.Inquiry.from_way.value = "|" + isMobile + "|";
+    window.document.Inquiry.refer.value = window.location.href + "?data=" + datenow + "&rnd=" + str + "&sys=" +
+        isMobile;
+    window.document.Inquiry.action = window.document.Inquiry.action + "?data=" + datenow + "&rnd=" + str + "&sys=" +
+        isMobile;
+    document.getElementById("button_sub").disabled = true;
+    window.document.Inquiry.submit();
+
+}
